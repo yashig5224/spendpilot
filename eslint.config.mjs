89fd5b267b4e698f46b8ffab67e6855622dab2ac
Yashi <yashigupta052422@gmail.com>
2026-05-25 16:13:20 +0530

    Fix ESLint setup

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+import nextPlugin from "@next/eslint-plugin-next";
+
+export default [
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+
+    languageOptions: {
+      globals: globals.browser,
+    },
+
+    plugins: {
+      react: reactPlugin,
+      "@next/next": nextPlugin,
+    },
+
+    rules: {
+      ...reactPlugin.configs.flat.recommended.rules,
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
